72eab7145ba24d3fb9b2333b4662978c47a1c7dd
Prakruti-Ukani <77276349+Prakruti-Ukani@users.noreply.github.com>
2021-04-16 21:32:21 +0530

    make textbox disabled on radio button click

--- MVC_3/user/add_note.php
+++ MVC_3/user/add_note.php
@@ -32,8 +32,16 @@ if(isset($_GET['note_id']))
         $code=$_POST['code'];
         $professor=$_POST['professor'];
         $mode=$_POST['mode'];
-        $price=$_POST['price'];
 
+		if(isset($_POST['price']))
+		{
+			$price=$_POST['price'];	
+		}        
+		else
+		{
+			$price=0;
+		}
+        
         $update_get_data=mysqli_query($conn,"UPDATE seller_note SET title='$title',catagory=$category,number_of_pages=$pages,description='$desc',university='$university',country=$country,course='$course',course_code='$code',professor='$professor',ispaid=$mode,note_type=$type,selling_price=$price WHERE id=$get_id;");
             
             if($update_get_data)
@@ -593,11 +601,13 @@ if(isset($_POST['publish-yes']))
                         <div class="col-md-6">
                             <div class="form-group">
                                     <label>Sell For*</label><br>
-
-                                    <input class="form-radio" type="radio" id="free" name="mode" style="height:20px; width:20px;" value="5"  <?=  (isset($_GET['note_id'])?(($db_mode==5)?"checked":' ') : ' ')?>>
-                                    <span class="sell-label">Free</span>
+                                    <label for="free">
+	                                    <input class="form-radio" type="radio" id="free" name="mode" style="height:20px; width:20px;" value="5"  <?=  (isset($_GET['note_id'])?(($db_mode==5)?"checked":' ') : ' ')?>>
+	                                    <span class="sell-label">Free</span>
+	                                </label>
+	                                <label for="paid">
                                     <input class="form-radio" type="radio" id="paid" name="mode" style="height:20px; width:20px;" value="4" <?=  (isset($_GET['note_id'])?(($db_mode==4)?"checked":' ') : ' ')?>><span class="sell-label">Paid</span>
-                                    
+                                    </label>
                             </div>
                             <div class="form-group">
                                     <label>Sell Price</label>
@@ -671,7 +681,20 @@ if(isset($_POST['publish-yes']))
                 $("#confirm-publish").modal('show');
             }
         </script>
-        
+    	<script type="text/javascript">
+		    $(function () {
+		        $("input[name='mode']").click(function () {
+		            if ($("#paid").is(":checked")) {
+		                $("#price").removeAttr("disabled");
+		                $("#price").focus();
+		            } else {
+		            	$("#price").attr("value","0");
+		                $("#price").attr("disabled", "disabled");
+		            }
+
+		        });
+		    });
+		</script>    
     </body>
 </html> 
 <?php }else{
